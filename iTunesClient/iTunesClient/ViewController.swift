e0310201095d6be199976c9c4e309d5ab5ce7bf9
Steve Wall <syntakks@gmail.com>
2020-02-12 10:46:12 -0600

    Initial Commit
    First Commit

---
+++ iTunesClient/iTunesClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesClient
+//
+//  Created by Stephen Wall on 2/12/20.
+//  Copyright © 2020 Stephen Wall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
